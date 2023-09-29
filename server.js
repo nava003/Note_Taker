92af82056d45c634572b7d5cc2e828fd17331d39
NavaHub <navaa003@gmail.com>
2023-09-29 04:32:13 -0500

    Added POST route logic when text title and field are populated with characters; db.json shows passing note content but the current POST logic does not handle refreshing the newly-populated db; current version of assignment is not modularized yet

--- server.js
+++ server.js
@@ -23,7 +23,37 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.post('/api/notes', (req, res) => {
-    
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+        
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNote, null, 4), (err) => {
+                    err ? console.error(err) : console.log('A new note has been added to db.json!')
+                })
+            }
+        })
+        
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        }
+
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in posting review');
+    }
 })
 
 app.delete('/api/notes/:id', (req, res) => {
