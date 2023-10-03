20049f37faba267f5bdd4d864eff63f265e4c4cf
NavaHub <navaa003@gmail.com>
2023-10-03 01:18:14 -0500

    Reorganized get notes logic using try/catch; try/catch in post notes has been correctly formatted; applied delete notes logic by grabbing the id property value, reading the db.json file, parsing it to find the matching id value in the object array, splicing out the found object, and re-writing the file with the edited parsed db, followed by standard server logic to indicate the procedure was a success.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -19,45 +19,69 @@ app.get('/notes', (req, res) => {
 })
 
 app.get('/api/notes', async (req, res) => {
-    let dbData = await fs.readFile('./db/db.json', 'utf-8');
-    const parDBData = JSON.parse(dbData);
-    return res.json(parDBData);
+    try {
+        let dbData = await fs.readFile('./db/db.json', 'utf-8');
+        const parDBData = JSON.parse(dbData);
+        
+        return res.json(parDBData);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('Error in retrieving notes.');
+    }
 })
 
 app.post('/api/notes', async (req, res) => {
-    const { title, text } = req.body;
+    try {
+        const { title, text } = req.body;
 
-    if (title && text) {
-        try {
+        if (title && text) {
             const newNote = {
                 id: uuidv4(),
                 title,
                 text,
             };
-            
+
             let readFS = await fs.readFile('./db/db.json', 'utf-8');
-            
             const parsedNote = JSON.parse(readFS);
             parsedNote.push(newNote);
-    
+
             await fs.writeFile('./db/db.json', JSON.stringify(parsedNote, null, 4));
-    
+
             const response = {
                 status: 'Success',
                 body: newNote,
             }
-    
+
             console.log(response.status, '\nNew Note has been saved to db.json!');
-            res.status(200).json(response);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json('Error in posting review');
+            res.status(201).json(response);
         }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('Error in posting note.');
     }
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-    
+app.delete('/api/notes/:id', async (req, res) => {
+    try {
+        const noteID = req.params.id;
+
+        const noteDB = await fs.readFile('./db/db.json', 'utf-8');
+        let parsedNoteDB = JSON.parse(noteDB);
+        const noteIndex = parsedNoteDB.findIndex(n => n.id === noteID);
+        parsedNoteDB.splice(noteIndex, 1);
+
+        await fs.writeFile('./db/db.json', JSON.stringify(parsedNoteDB, null, 4));
+
+        const response = {
+            status: 'Success'
+        }
+
+        console.log(response.status, '\nNote has been deleted from db.json!');
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('Error in deleting note.');
+    }
 })
 
 app.get('*', (req, res) => {
